b758cd7cf015b161da3f728f438dc1be2bd8425f
Ishan Patil <ishanp141@gmail.com>
2025-12-12 19:42:40 +0530

    Add files via upload

---
+++ descriptions.py
@@ -0,0 +1,14 @@
+top_md = """
+# VALL-E X  
+VALL-E X can synthesize high-quality personalized speech with only a 3-second enrolled recording of 
+an unseen speaker as an acoustic prompt for english language<br>
+"""
+
+infer_from_audio_md = """
+Upload a speech of 3~10 seconds as the audio prompt and type in the text you'd like to synthesize.<br>
+The model will synthesize speech of given text with the same voice of your audio prompt.<br>
+The model also tends to preserve the emotion & acoustic environment of your given speech.<br>
+"""
+
+
+
